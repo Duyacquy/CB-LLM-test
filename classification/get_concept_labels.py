5999f09fccdfaa1786d3a3b3cad789045bd1180b
duyacquy <duytran2552005@gmail.com>
2026-01-08 18:38:58 +0700

    add pubmed dataset

--- classification/get_concept_labels.py
+++ classification/get_concept_labels.py
@@ -48,7 +48,7 @@ def build_sim_loaders(encode_sim):
 
 print("--------------------------------GET_CONCEPT_LABEL--------------------------------")
 print("loading data...")
-train_dataset, val_dataset, test_dataset = train_val_test_split(args.dataset, CFG.dataset_config[args.dataset]["label_column"], ratio=0.2, has_val=True)
+train_dataset, val_dataset, test_dataset = train_val_test_split(args.dataset, CFG.dataset_config[args.dataset]["label_column"], ratio=0.2, has_val=False)
 
 concept_set = CFG.concept_set[args.dataset]
 print("concept len: ", len(concept_set))
